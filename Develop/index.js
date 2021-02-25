b29aba1f2017286258b2b58d1755dc64127cbbf7
Vinh Huynh <vinhkhamhuynh@gmail.com>
2021-02-24 23:43:48 -0500

    created write readme file , function to init app

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,48 @@
-// TODO: Include packages needed for this application
+// Packages needed 
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+//Created an array of questions for user input
+const promptUser = () =>
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'fullName',
+            message: 'What is your name ?',
+        },
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+        {
+            type: 'input',
+            name: 'GitHub',
+            message: 'What is your GitHub username ?',
+        },
 
-// TODO: Create a function to initialize app
-function init() {}
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address ?',
+        },
+
+    ]);
+
+//Created a function to write README file
+const generateReadme = (answers) =>
+    `# testing ${answers.fullName}\n## ${answers.GitHub}\n### ${answers.email}`;
+
+//Created a function to initialize app
+const init = () => {
+    promptUser()
+        .then((answers) => {
+            try {
+                const readme = generateReadme(answers);
+                fs.writeFileSync('../README.md', readme);
+                console.log('SUCCESSFULLY created README.md');
+            }
+            catch (error) { 
+                console.log(error); 
+            }
+        });
+};
 
 // Function call to initialize app
 init();
